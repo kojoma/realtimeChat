cd15eb8ea85d667764109d5343fb3bce824fefc4
masaru.kojo <masaru.kojo@mixi.co.jp>
2017-06-15 18:46:44 +0900

    ロビー表示時にDBに保存してたメッセージを表示するようにした

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule RealtimeChat.PageController do
   use RealtimeChat.Web, :controller
 
+  alias RealtimeChat.Message
+
   def index(conn, _params) do
-    render conn, "index.html"
+    # ロビーはroom_id=0
+    messages = Message.find_by_room(Message, 0)
+    render(conn, "index.html", messages: messages)
   end
 end
